49e98c4a7a3e48dc7a81bde30e66f54bf3e61a35
Joshua Cubilla <joshuacubilla2003@gmail.com>
2026-05-15 19:19:57 +0800

    make 7 days longer the refreshToken

--- server/src/services/auth.services.ts
+++ server/src/services/auth.services.ts
@@ -56,7 +56,7 @@ export const createRefreshToken = async (userId: number) => {
     await revokeAllUserTokens(userId) // ensure one active refresh token per user for better security
 
     const token = randomBytes(64).toString('hex')
-    const expiresAt = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000) // 1 day   
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days   
     
     const parsed = tokenValidation.tokenSchema.safeParse({ userId, token, expiresAt})
 
